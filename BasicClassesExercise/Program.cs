83ceca429a4a92c16de6d98dca83b7116ffc71d8
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-05-10 13:43:03 -0400

    ClassesExercise

---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyCar = new Car();
+            MyCar.Make = "Buick";
+            MyCar.Model = "Encore";
+            MyCar.Year = 2017;
+            
+            Console.WriteLine($"I have a {MyCar.Year} {MyCar.Make} {MyCar.Model}.");
+        }
+    }
+}
